ec778ed0d3e06e7b95aaf451cfac41ce258cea0e
Abhishek Yadav <159156426+abhishekkumar62000@users.noreply.github.com>
2025-06-30 20:05:45 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@
 logging.basicConfig(level=logging.DEBUG)
 
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 import yagmail # type: ignore
 import plotly.express as px
 from datetime import datetime 
