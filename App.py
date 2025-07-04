a23d79274e3a809088d3a1bd9de3f29ee5c28393
Abhishek Yadav <159156426+abhishekkumar62000@users.noreply.github.com>
2025-07-04 11:50:39 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import yagmail # type: ignore
 
-import plotly.express as px
+import plotly.express as px 
 from datetime import datetime
 import io
 import requests
